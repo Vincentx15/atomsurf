78bb7173a965183260c8d3643b65ff88fd20dfb9
pvnieo <sohaib.attaiki@gmail.com>
2024-04-23 20:25:53 +0200

    add vanilla config

--- atomsurf/network_utils/__init__.py
+++ atomsurf/network_utils/__init__.py
@@ -1,17 +1,25 @@
+from .communication.blocks import ConcurrentCommunication, SequentialCommunication, ParallelCommunicationV1, SequentialCommunicationV1, GATCommunicationV1
+from .diffusion_net.diffusion_net import DiffusionNet, DiffusionNetBlock
+from .diffusion_net.diffusion_net_batch import DiffusionNetBatch, DiffusionNetBlockBatch
+from .misc_arch.graph_blocks import GCNx2Block
 from .misc_arch.deltaconv import DeltaConv
 from .misc_arch.dgcnn import DGCNN, DGCNNLayer
 from .misc_arch.pointnet import PointNet
-from .diffusion_net.diffusion_net import DiffusionNet, DiffusionNetBlock
-from .diffusion_net.diffusion_net_batch import DiffusionNetBatch, DiffusionNetBlockBatch
 
 
 __all__ = [
-    "DeltaConv",
-    "DGCNN",
-    "DGCNNLayer",
-    "PointNet",
+    "ConcurrentCommunication",
+    "SequentialCommunication",
     "DiffusionNet",
     "DiffusionNetBlock",
     "DiffusionNetBatch",
     "DiffusionNetBlockBatch",
+    "GCNx2Block",
+    "DeltaConv",
+    "DGCNN",
+    "DGCNNLayer",
+    "PointNet",
+    "ParallelCommunicationV1",
+    "SequentialCommunicationV1",
+    "GATCommunicationV1",
 ]
