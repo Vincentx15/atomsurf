502ae77c802589e3ef2d432b225eacdcdd30d06d
pvnieo <sohaib.attaiki@gmail.com>
2024-04-23 21:56:55 +0200

    load encoder inside pl model

--- atomsurf/tasks/masif_ligand/pl_model.py
+++ atomsurf/tasks/masif_ligand/pl_model.py
@@ -1,11 +1,15 @@
-# import torch
+# 3p
 import pytorch_lightning as pl
+# project
+from atomsurf.networks.protein_encoder import ProteinEncoder
 
 
 class MasifLigandModule(pl.LightningModule):
     def __init__(self, hparams) -> None:
         super().__init__()
         self.save_hyperparameters()
+
+        self.encoder = ProteinEncoder(hparams)
 
     def forward(self, x):
         pass
