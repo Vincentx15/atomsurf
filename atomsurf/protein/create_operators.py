Improve the no-pymesh computation, with a very cheap fix of catching normal norm errors to avoid IGL Segfault.

atomsurf/protein/create_operators.py
@@ -158,20 +158,21 @@ def mesh_vertex_normals(verts, faces):
     vertex_normals = np.zeros_like(verts)
     for i in range(3):
         np.add.at(vertex_normals, faces[:, i], face_n)
-
-    vertex_normals = vertex_normals / np.linalg.norm(
-        vertex_normals, axis=-1, keepdims=True
-    )
-
+    norm = np.linalg.norm(vertex_normals, axis=-1, keepdims=True)
+    vertex_normals = vertex_normals / norm
     return vertex_normals
 
 
-def vertex_normals(verts, faces):
+def vertex_normals(verts, faces, permissive=False):
     normals = mesh_vertex_normals(verts, faces)
 
     # if any are NaN, wiggle slightly and recompute
     bad_normals_mask = np.isnan(normals).any(axis=1, keepdims=True)
-    if bad_normals_mask.any():
+    bad_normals = bad_normals_mask.any()
+    if bad_normals:
+        if not permissive:
+            raise ValueError(f"Some normals are nan, indicating problems with the mesh and potential ulterior "
+                             f"segmentation fault later in the code.")
         bbox = np.amax(verts, axis=0) - np.amin(verts, axis=0)
         scale = np.linalg.norm(bbox) * 1e-4
         wiggle = (np.random.RandomState(seed=777).rand(*verts.shape) - 0.5) * scale

atomsurf/protein/create_surface.py
@@ -228,6 +228,14 @@ def mesh_simplification(verts, faces, out_ply,
                         min_vert_number=140,
                         max_vert_number=50000,
                         use_pymesh=True):
+    """
+    Simplify and clean a mesh.
+    Simplification is based on coarsening with iterative quadratic decimation
+    Cleaning involves discarding small disconnected components, unreferenced faces and vertices...
+
+    Most of the computation time resides in the coarsening.
+    """
+
     # remeshing to have a target number of vertices
     faces_num = max(min_vert_number * 2 + 1, int(face_reduction_rate * len(faces)))
     mesh = o3d.geometry.TriangleMesh(o3d.utility.Vector3dVector(verts), o3d.utility.Vector3iVector(faces))
@@ -243,8 +251,23 @@ def mesh_simplification(verts, faces, out_ply,
     mesh.remove_unreferenced_vertices()
 
     mesh = mesh.simplify_quadric_decimation(target_number_of_triangles=faces_num)
+
     verts_out = np.asarray(mesh.vertices)
     faces_out = np.asarray(mesh.triangles)
+
+    if not use_pymesh:
+        # TODO time
+        triangle_clusters, cluster_n_triangles, cluster_area = (mesh.cluster_connected_triangles())
+        triangle_clusters = np.asarray(triangle_clusters)
+        cluster_n_triangles = np.asarray(cluster_n_triangles)
+        cluster_area = np.asarray(cluster_area)
+
+        # Some tiny clusters might exist, we want to remove those
+        assert (cluster_n_triangles >= 10).sum() == 1
+        triangles_to_remove = cluster_n_triangles[triangle_clusters] < 10
+        mesh.remove_triangles_by_mask(triangles_to_remove)
+        mesh.remove_unreferenced_vertices()
+        mesh.remove_degenerate_triangles()
 
     if use_pymesh:
         import pymesh
@@ -371,4 +394,4 @@ if __name__ == "__main__":
                                out_ply_path="../../data/example_files/example_mesh.ply",
                                min_vert_number=140,
                                use_pymesh=False,
-                               face_reduction_rate=1)
+                               face_reduction_rate=0.1)