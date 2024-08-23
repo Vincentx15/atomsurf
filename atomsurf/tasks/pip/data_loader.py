An accidental debug push happened, also fix accuracy computation

atomsurf/tasks/pip/data_loader.py
@@ -30,6 +30,7 @@ class GraphBuilderPIP(GraphLoader):
         self.data_dir = os.path.join(config.data_dir, mode, 'rgraph')
         self.esm_dir = os.path.join(config.data_dir, mode, 'esm')
 
+
 class PIPDataset(Dataset):
     def __init__(self, data_dir, surface_builder, graph_builder, neg_to_pos_ratio=1, max_pos_regions_per_ensemble=-1):
         self.systems = LMDBDataset(data_dir)
@@ -102,7 +103,7 @@ class PIPDataset(Dataset):
             return None
         if idx_left.dtype != torch.int64 and idx_right.dtype != torch.int64:
             return None
-        if idx_left.max() >=len(graph_1.node_pos) or idx_right.max() >= len(graph_2.node_pos):
+        if idx_left.max() >= len(graph_1.node_pos) or idx_right.max() >= len(graph_2.node_pos):
             print('idx error', names)
             return None
         # locs_left = graph_1.node_pos[idx_left]
@@ -110,7 +111,9 @@ class PIPDataset(Dataset):
         # TODO MISS transform and normalize
         # item = Data(surface_1=surface_1, graph_1=graph_1,surface_2=surface_2, graph_2=graph_2, idx_left=idx_left,idx_right=idx_right, label=labels)
 
-        item = Data(surface_1=surface_1, graph_1=graph_1,surface_2=surface_2, graph_2=graph_2, idx_left=idx_left,idx_right=idx_right, label=labels,g1_len=graph_1.node_pos.shape[0],g2_len=graph_2.node_pos.shape[0])
+        item = Data(surface_1=surface_1, graph_1=graph_1, surface_2=surface_2, graph_2=graph_2, idx_left=idx_left,
+                    idx_right=idx_right, label=labels, g1_len=graph_1.node_pos.shape[0],
+                    g2_len=graph_2.node_pos.shape[0])
         # item = Data(surface_1=surface_1, graph_1=graph_1, surface_2=surface_2, graph_2=graph_2, locs_left=locs_left,
         #             locs_right=locs_right, labels_pip=labels)
         # print('process one data ',time.time()-t0)
@@ -128,7 +131,6 @@ class PIPDataModule(pl.LightningDataModule):
         self.graph_builders = []
         self.cfg = cfg
         for mode in ['train', 'val', 'test']:
-            mode = 'test'
             self.systems.append(os.path.join(data_dir, mode))
             self.surface_builders.append(SurfaceLoaderPIP(self.cfg.cfg_surface, mode=mode))
             self.graph_builders.append(GraphBuilderPIP(self.cfg.cfg_graph, mode=mode))
@@ -152,7 +154,8 @@ class PIPDataModule(pl.LightningDataModule):
         return DataLoader(dataset, shuffle=False, **self.loader_args)
 
     def test_dataloader(self):
-        dataset = PIPDataset(self.systems[2], self.surface_builder_test, self.graph_builder_test,max_pos_regions_per_ensemble=5)
+        dataset = PIPDataset(self.systems[2], self.surface_builder_test, self.graph_builder_test,
+                             max_pos_regions_per_ensemble=5)
         return DataLoader(dataset, shuffle=False, **self.loader_args)
 
 

atomsurf/tasks/pip/pl_model.py
@@ -3,57 +3,32 @@ import sys
 
 import pytorch_lightning as pl
 import torch
-import torch.nn.functional as F
 
 # project
 from atomsurf.tasks.pip.model import PIPNet
 from atomsurf.utils.metrics import compute_auroc, compute_accuracy
-from sklearn.metrics import roc_auc_score
-
-def compute_accuracy(predictions, labels):
-    # Convert predictions to binary labels (0 or 1)
-    predictions = torch.sigmoid(predictions)
-    predicted_labels = torch.round(predictions)
-    # Compare predicted labels with ground truth labels
-    correct_count = (predicted_labels == labels).sum().item()
-    total_count = labels.size(0)
-    # Compute accuracy
-    accuracy = correct_count / total_count
-    return accuracy
 
 
-def compute_auroc(predictions, labels):
-    labels = labels.detach().cpu().numpy()
-    predictions = torch.sigmoid(predictions)
-    predictions = predictions.detach().cpu().numpy()
-    try:
-        auroc = roc_auc_score(y_true=labels, y_score=predictions)
-        return auroc
-    except ValueError as e:
-        print("Auroc computation failed, ", e)
-        return 0.5
-    
 class PIPModule(pl.LightningModule):
     def __init__(self, hparams) -> None:
         super().__init__()
         self.save_hyperparameters()
-        self.criterion = torch.nn.BCEWithLogitsLoss()#pos_weight=torch.tensor([hparams.model.pos_weight])
-        self.model = PIPNet(hparams_encoder=hparams.encoder, hparams_head=hparams.cfg_head,use_graph_only=True)
-
+        self.criterion = torch.nn.BCEWithLogitsLoss()  # pos_weight=torch.tensor([hparams.model.pos_weight])
+        self.model = PIPNet(hparams_encoder=hparams.encoder, hparams_head=hparams.cfg_head, use_graph_only=True)
 
     def forward(self, x):
         return self.model(x)
 
     def step(self, batch):
 
-        if batch is None :
+        if batch is None:
             return None, None, None
-        if len(set(batch.graph_1.batch.cpu().numpy()))<2:
+        if len(set(batch.graph_1.batch.cpu().numpy())) < 2:
             return None, None, None
         if isinstance(batch.label, list):
-            labels = torch.cat(batch.label).reshape(-1,1)
+            labels = torch.cat(batch.label).reshape(-1, 1)
         else:
-            labels = batch.label.reshape(-1,1)
+            labels = batch.label.reshape(-1, 1)
         outputs = self(batch)
         loss = self.criterion(outputs, labels)
         # if torch.isnan(loss).any():
@@ -61,14 +36,13 @@ class PIPModule(pl.LightningModule):
         #     return None, None, None
         return loss, outputs, labels
 
-
     def training_step(self, batch, batch_idx):
         loss, logits, labels = self.step(batch)
         if loss is None:
             return None
         self.log_dict({"loss/train": loss.item()},
                       on_step=True, on_epoch=True, prog_bar=False, batch_size=len(logits))
-        acc = compute_accuracy(logits, labels)
+        acc = compute_accuracy(logits, labels, add_sigmoid=True)
         auroc = compute_auroc(logits, labels)
         self.log_dict({"acc/train": acc, "auroc/train": auroc}, on_epoch=True, batch_size=len(logits))
         return loss
@@ -82,7 +56,7 @@ class PIPModule(pl.LightningModule):
             return None
         self.log_dict({"loss/val": loss.item()},
                       on_step=False, on_epoch=True, prog_bar=True, batch_size=len(logits))
-        acc = compute_accuracy(logits, labels) # TODO FIX
+        acc = compute_accuracy(logits, labels, add_sigmoid=True)
         auroc = compute_auroc(logits, labels)
         self.log_dict({"acc/val": acc, "auroc/val": auroc}, on_epoch=True, batch_size=len(logits))
         self.log("auroc_val", auroc, prog_bar=True, on_step=False, on_epoch=True, logger=False, batch_size=len(logits))
@@ -95,7 +69,7 @@ class PIPModule(pl.LightningModule):
             return None
         self.log_dict({"loss/test": loss.item()},
                       on_step=False, on_epoch=True, prog_bar=True, batch_size=len(logits))
-        acc = compute_accuracy(logits, labels) # TODO FIX
+        acc = compute_accuracy(logits, labels, add_sigmoid=True)
         auroc = compute_auroc(logits, labels)
         self.log_dict({"acc/test": acc, "auroc/test": auroc}, on_epoch=True, batch_size=len(logits))
 

atomsurf/tasks/psr/pl_model.py
@@ -1,16 +1,15 @@
 import os
 import sys
 
-import pytorch_lightning as pl
-import torch
-import torch.nn.functional as F
 from collections import defaultdict
+import numpy as np
+import pytorch_lightning as pl
+import scipy
+import torch
+
 # project
 from atomsurf.tasks.psr.model import PSRNet
-from atomsurf.utils.metrics import compute_auroc, compute_accuracy
-from sklearn.metrics import roc_auc_score
-import numpy as np
-import scipy
+
 
 def safe_spearman(gt, pred):
     if np.all(np.isclose(pred, pred[0])):
@@ -31,13 +30,14 @@ def rs_metric(reslist, resdict):
         local_r.append(safe_spearman(gt, pred))
     local_r = float(np.mean(local_r))
     return global_r, local_r
-    
+
+
 class PSRModule(pl.LightningModule):
     def __init__(self, hparams) -> None:
         super().__init__()
         self.save_hyperparameters()
         self.criterion = torch.nn.MSELoss()
-        self.model = PSRNet(hparams_encoder=hparams.encoder, hparams_head=hparams.cfg_head,use_graph_only=True)
+        self.model = PSRNet(hparams_encoder=hparams.encoder, hparams_head=hparams.cfg_head, use_graph_only=True)
         self.val_reslist = list()
         self.val_resdict = defaultdict(list)
 
@@ -49,22 +49,21 @@ class PSRModule(pl.LightningModule):
 
     def step(self, batch):
 
-        if batch is None :
+        if batch is None:
             return None, None, None
-        if len(set(batch.graph.batch.cpu().numpy()))<2:
+        if len(set(batch.graph.batch.cpu().numpy())) < 2:
             return None, None, None
-        scores = batch.score.reshape(-1,1)
+        scores = batch.score.reshape(-1, 1)
         outputs = self(batch)
         loss = self.criterion(outputs, scores)
         names = batch.name
         if torch.isnan(loss).any():
             print('Nan loss')
             return None, None, None
-        return loss, outputs, scores,names
-
+        return loss, outputs, scores, names
 
     def training_step(self, batch, batch_idx):
-        loss, logits, scores,names = self.step(batch)
+        loss, logits, scores, names = self.step(batch)
         if loss is None:
             return None
         self.log_dict({"loss/train": loss.item()},
@@ -73,7 +72,7 @@ class PSRModule(pl.LightningModule):
 
     def validation_step(self, batch, batch_idx: int):
         self.model.eval()
-        loss, logits, scores,names = self.step(batch)
+        loss, logits, scores, names = self.step(batch)
         if loss is None or logits.isnan().any() or scores.isnan().any():
             return None
         for name, logit, score in zip(names, logits, scores):
@@ -85,7 +84,7 @@ class PSRModule(pl.LightningModule):
 
     def test_step(self, batch, batch_idx: int):
         self.model.eval()
-        loss, logits, scores,names = self.step(batch)
+        loss, logits, scores, names = self.step(batch)
         if loss is None or logits.isnan().any() or scores.isnan().any():
             return None
         for name, logit, score in zip(names, logits, scores):
@@ -94,7 +93,6 @@ class PSRModule(pl.LightningModule):
             self.test_resdict[name[0:5]].append(reslist)
         self.log_dict({"loss/test": loss.item()},
                       on_step=False, on_epoch=True, prog_bar=True, batch_size=len(logits))
-
 
     def configure_optimizers(self):
         opt_params = self.hparams.hparams.optimizer
@@ -127,7 +125,6 @@ class PSRModule(pl.LightningModule):
         self.log_dict({"global_r/val": global_r})
         self.log_dict({"local_r/val": local_r})
         self.log("global_r_val", global_r, prog_bar=True, on_step=False, on_epoch=True, logger=False)
-
 
     def on_test_epoch_end(self) -> None:
         global_r, local_r = rs_metric(self.test_reslist, self.test_resdict)

atomsurf/utils/metrics.py
@@ -5,8 +5,10 @@ import torch
 from torchmetrics.functional import accuracy, precision, recall, f1_score, auroc
 
 
-def compute_accuracy(predictions, labels):
+def compute_accuracy(predictions, labels, add_sigmoid=False):
     # Convert predictions to binary labels (0 or 1)
+    if add_sigmoid:
+        predictions = torch.sigmoid(predictions)
     predicted_labels = torch.round(predictions)
     # Compare predicted labels with ground truth labels
     correct_count = (predicted_labels == labels).sum().item()